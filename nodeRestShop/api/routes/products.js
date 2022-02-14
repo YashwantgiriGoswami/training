ef8bb94b63f29fb71da432f5fb8f97349bc5506b
YashwantgiriGoswami <yashwantgirig@zignuts.com>
2022-02-14 19:37:04 +0530

    practice restful API

--- nodeRestShop/api/routes/products.js
+++ nodeRestShop/api/routes/products.js
@@ -1,6 +1,58 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const route = express.Router();
+const Products = require('/home/ztlab/Documents/Assignments/nodeRestShop/models/product');
 
-route.get('/',(req,res,next)=>{
-    
+route.get('/', (req, res, next) => {
+
+    Products.find().then((result) => {
+
+        res.status(200).json(result);
+
+    });
 });
+
+route.post('/', (req, res, next) => {
+
+    var { name, quantity } = req.body;
+
+    const product = new Products({
+        _id: new mongoose.Types.ObjectId(),
+        name: name,
+        quantity: quantity
+    });
+
+    product.save().then((result) => {
+        console.log(result);
+    });
+
+    res.status(200).json({
+        message: `in post request product`,
+        name: name,
+        quantity: quantity
+
+    });
+});
+
+route.get('/:name', (req, res, next) => {
+
+    Products.find({ name: req.params.name }).then((result) => {
+        res.status(200).json(result);
+    });
+});
+
+route.delete('/:name', (req, res, next) => {
+
+    Products.remove({ name: req.params.name }).then((result) => {
+        res.status(200).json(result);
+    });
+});
+
+route.patch('/', (req, res, next) => {
+
+    res.status(201).json({
+        message: `in patch request product`
+    });
+});
+
+module.exports = route;
--- nodeRestShop/app.js
+++ nodeRestShop/app.js
@@ -1,9 +1,56 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const product = require('./api/routes/products');
+const order = require('./api/routes/order');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    res.send("yashwant is good.");
+mongoose.connect('mongodb+srv://yashwant:yashwant@yashwant.us1kn.mongodb.net/Product?retryWrites=true&w=majority');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+
+//     res.header('Access-Control-Allow-Origin', "*");
+
+//     res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+//     if (req.method === 'OPTIONS') {
+
+//         res.header('Access-Control-Allow-Method', 'PUT,POST,DELETE,PATCH,GET');
+//         return res.status(200).json({});
+//     }
+//     next();
+
+// });
+
+app.use('/product', product);
+app.use('/order', order);
+
+app.use((req, res, next) => {
+
+    const error = new Error('Page not found');
+    error.status = 404;
+    next(error);
+
 });
 
-module.exports =app;
+app.use((error, req, res, next) => {
+
+    res.status(error.status || 500);
+
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+
+
+});
+
+module.exports = app;
